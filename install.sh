a5cdc709eea9c9d7f1d876b7dbb1d9829c2e5665
Davide Setti <setti@spaziodati.eu>
2020-12-24 14:44:49 +0100

    add zsh conf

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 ln -s -i $PWD/nvimrc /home/vad/.config/nvim/init.vim
 ln -s -i $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
+
+ln -s -i $PWD/zsh/zshrc ~/.zshrc
+ln -s -i $PWD/zsh/zsh ~/.zsh
