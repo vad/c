053ceb5988876329dbbf4fdc6760b2fced00e6ce
Davide Setti <setti@spaziodati.eu>
2020-12-24 15:02:32 +0100

    p10k

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -s -i $PWD/nvimrc /home/vad/.config/nvim/init.vim
 ln -s -i $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
 
 ln -s -i $PWD/zsh/zshrc ~/.zshrc
-ln -s -i $PWD/zsh/zsh ~/.zsh
+#ln -s -i $PWD/zsh/zsh .zsh -t $HOME
+ln -s -i $PWD/zsh/p10k.zsh ~/.p10k.zsh
