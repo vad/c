7d5d2ba58e9095eaf0f11f2d9fe12eeef60fb44f
Davide Setti <setti@spaziodati.eu>
2020-12-24 14:48:39 +0100

    add zsh conf

---
+++ zsh/zsh/alias.zsh
@@ -0,0 +1,27 @@
+alias ls='ls --color=auto'
+alias ll='ls -la'
+
+alias grep='grep --color'
+
+alias pull="git pull --rebase"
+
+alias vim="nvim -O"
+
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
+
+alias cdg='cd $(git rev-parse --show-toplevel)'
+
+alias o='xdg-open'
+
+alias serve='caddy -port 8000 browse "log stdout"'
+
+alias av='aws-vault'
+alias ave='aws-vault exec'
+# alias pim="aws-vault exec --assume-role-ttl=1h sd -- pim"
+# alias kim="aws-vault exec --assume-role-ttl=1h sd -- pim k8s"
+
+alias tf=terraform
+alias k=kubectl
+
+alias bfg="java -jar ~/data/Software/bfg-1.13.0.jar"
