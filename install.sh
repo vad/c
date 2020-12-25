2f5ac8d5571a576ec54d1ed68d6d43f8369d8029
Davide Setti <setti@spaziodati.eu>
2020-12-25 14:59:11 +0100

    py3status

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ ln -s -i $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -s -i $PWD/zsh/zshrc ~/.zshrc
 #ln -s -i $PWD/zsh/zsh .zsh -t $HOME
 ln -s -i $PWD/zsh/p10k.zsh ~/.p10k.zsh
+
+mkdir -p ~/.config/py3status
+ln -s -i $PWD/py3status ~/.config/py3status/config
