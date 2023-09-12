f06080bc7e0345cd3fd41b66195d7dd7e2107573
Davide Setti <setti@spaziodati.eu>
2023-09-12 09:17:51 +0200

    migrate to lazy.vim package manager

--- nvim/lua/vad/opts.lua
+++ nvim/lua/vad/opts.lua
@@ -6,15 +6,14 @@ opt.number = true
 opt.relativenumber = true
 
 opt.termguicolors = true
-vim.cmd.colorscheme("gruvbox")
 
 opt.expandtab = true
 opt.list = true
 
-opt.hlsearch = true         -- Hilight searching
-opt.ignorecase = true       -- Case insensitive
-opt.incsearch = true        -- Search as you type
-opt.infercase = true        -- Completion recognizes capitalization
+opt.hlsearch = true -- Hilight searching
+opt.ignorecase = true -- Case insensitive
+opt.incsearch = true -- Search as you type
+opt.infercase = true -- Completion recognizes capitalization
 
 opt.shiftwidth = 4
 opt.tabstop = 4
--- nvim/lua/vad/packages.lua
+++ nvim/lua/vad/packages.lua
@@ -1,40 +1,50 @@
 local vim = vim
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost packages.lua source <afile> | PackerCompile
-  augroup end
-]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+require("lazy").setup({
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
 
-	use({ "ellisonleao/gruvbox.nvim" }) -- lua version, faster
-
-	use({
+	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
 		end,
-	})
+	},
 
-	use({
+	{
 		"tpope/vim-fugitive",
-		requires = { "shumphrey/fugitive-gitlab.vim" },
-	})
+		dependencies = { "shumphrey/fugitive-gitlab.vim" },
+	},
 
-	use({
+	{
 		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons" },
-	})
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-	use("ntpeters/vim-better-whitespace")
+	"ntpeters/vim-better-whitespace",
 
-	use({
+	{
 		"nvim-tree/nvim-tree.lua",
-		requires = {
+		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
@@ -43,66 +53,66 @@ return require("packer").startup(function(use)
 				hijack_netrw = true,
 			})
 		end,
-	})
+	},
 
-	use({
+	{
 		"tpope/vim-surround",
-		requires = { "tpope/vim-repeat" },
-	})
+		dependencies = { "tpope/vim-repeat" },
+	},
 
-	use("vim-scripts/restore_view.vim")
+	"vim-scripts/restore_view.vim",
 
-	use("tpope/vim-commentary")
+	"tpope/vim-commentary",
 
 	-- filetypes
-	use("pearofducks/ansible-vim")
-	use("amadeus/vim-mjml")
-	use("hashivim/vim-terraform")
-	use("fatih/vim-go")
-	use("robbles/logstash.vim")
-	use("uarun/vim-protobuf")
-	use("ziglang/zig.vim")
+	"pearofducks/ansible-vim",
+	"amadeus/vim-mjml",
+	"hashivim/vim-terraform",
+	"fatih/vim-go",
+	"robbles/logstash.vim",
+	"uarun/vim-protobuf",
+	"ziglang/zig.vim",
 
 	-- lsp
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
 
 	-- Install nvim-cmp
-	use("hrsh7th/nvim-cmp")
+	"hrsh7th/nvim-cmp",
 	-- Install snippet engine (This example installs [hrsh7th/vim-vsnip](https://github.com/hrsh7th/vim-vsnip))
-	use("hrsh7th/cmp-vsnip")
-	use("hrsh7th/vim-vsnip")
-	use("hrsh7th/vim-vsnip-integ")
+	"hrsh7th/cmp-vsnip",
+	"hrsh7th/vim-vsnip",
+	"hrsh7th/vim-vsnip-integ",
 
 	-- Install the buffer completion source
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-nvim-lua")
-	use("ray-x/cmp-treesitter")
-	use("f3fora/cmp-spell")
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-nvim-lua",
+	"ray-x/cmp-treesitter",
+	"f3fora/cmp-spell",
 
 	-- This tiny plugin adds vscode-like pictograms to neovim built-in lsp
-	use("onsails/lspkind-nvim")
+	"onsails/lspkind-nvim",
 
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("nvim-treesitter/playground")
-	use("nvim-treesitter/nvim-treesitter-textobjects")
-	use("nvim-treesitter/nvim-treesitter-context")
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"nvim-treesitter/playground",
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	"nvim-treesitter/nvim-treesitter-context",
 
-	use({ "mhartington/formatter.nvim" })
+	{ "mhartington/formatter.nvim" },
 
-	use({
+	{
 		"nvim-telescope/telescope.nvim",
-		requires = {
+		dependencies = {
 			{ "nvim-lua/plenary.nvim" },
 			{ "nvim-telescope/telescope-live-grep-args.nvim" },
 		},
-	})
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 
-	use({ "j-hui/fidget.nvim", tag = "legacy" })
+	{ "j-hui/fidget.nvim", tag = "legacy" },
 
-	use({ "dstein64/vim-startuptime" })
-end)
+	"dstein64/vim-startuptime",
+})
